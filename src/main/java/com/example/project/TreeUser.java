fec8ecdbe07abfbab9df77689c071c84cf224373
moaddreem <moaddreem0505@gmail.com>
2024-11-23 15:30:36 +0300

    Update TreeUser.java
    i know countleaves is boolean but testing

--- src/main/java/com/example/project/TreeUser.java
+++ src/main/java/com/example/project/TreeUser.java
@@ -1,11 +1,14 @@
 package com.example.project;
 
 public class TreeUser {
-    public static <T> int countLeaves(BT<T> bt) {
-        throw new UnsupportedOperationException("Not supported yet.");
-		// Write the method countLeafs that should return the number of leaf nodes in the tree. A leaf node is a node that has no children.
-        // assume the following method exists in the ADT: isLeaf (boolean flag): requires: Binary tree is not empty.
-        // input: None. results: if the current node of the binary tree is a leaf then flag is set to true otherwise it is set to false. output: flag.
-        // you can't call countLeaves from BT.java
+	public static <T> int countLeaves(BT<T> bt) {
+    	if(bt.empty())
+    		return -1;
+    	if(bt == null)
+    		return 0;
+    	if(bt.isLeaf())
+    		return 1;
+    	return countLeaves(bt.find(Relative.LeftChild))+countLeaves(bt.find(Relative.RightChild));
     }
+
 }
